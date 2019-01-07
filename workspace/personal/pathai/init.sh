d1c5372ec1ae8595941b56f16078523d642f6247
Matthew Coleman <matthew@eudaimonia.io>
2019-01-07 14:02:10 -0500

    Add the init script for Mac OSX environments

---
+++ workspace/personal/pathai/init.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# In case I ever need to rebuild.
+
+brew cask install \
+  aptible
+
+brew install \
+  argoproj/tap/argo \
+  automake \
+  coreutils \
+  direnv \
+  findutils \
+  go \
+  jq \
+  kubeless \
+  kubernetes-cli \
+  kubernetes-helm \
+  libtool \
+  libxmlsec1 \
+  npm \
+  pkg-config \
+  postgresql \
+  pyenv-virtualenv \
+  python3 \
+  shyaml \
+  telnet \
+  vault \
+  watch \
+  wget
+
+sudo easy_install pip
+
+pip install --user \
+  awscli \
+  flake8 \
+  pgsanity \
+  pytest \
+  virtualenv
